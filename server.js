9a2b6adc677f866496ce61da25ca6ffeca46ca32
aqeelmozumder <aqeelmozumder@hotmail.com>
2023-03-20 23:48:44 -0700

    Sample Form

--- server.js
+++ server.js
@@ -2,16 +2,17 @@
 
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+//Static files
+app.use(express.static("./public"));
 
-
-app.get('/', (req, res) => {
-    //res.redirect("/views/metricForm");
-    res.sendFile(__dirname + '/views/metricForm.ejs');
-    //res.send('Priority Based Family Physician Access');
- });
+app.get("/", (req, res) => {
+  //res.redirect("/views/metricForm");
+  res.render("metricForm.ejs");
+  //res.send('Priority Based Family Physician Access');
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
