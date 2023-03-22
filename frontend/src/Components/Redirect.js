e6b06bccbbf646815a5aa70ba9727e1172638685
Mustafa Wasif <mustafawasif@uvic.ca>
2023-03-22 13:57:36 -0700

    Removed ES Lint errors

--- frontend/src/Components/Redirect.js
+++ frontend/src/Components/Redirect.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Feedback } from "./Feedback";
 import PatientForm from "./PatientForm";
