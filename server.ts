eb78e4219e65db67118a87bd4f366eb6335a7e6b
Mustafa Wasif <mustafawasif@uvic.ca>
2023-03-24 22:50:32 -0700

    test

--- server.ts
+++ server.ts
@@ -9,17 +9,17 @@ const app = express(); //Create an instance of express app
 app.use(cors()); //Allow different domains to access endpoints in backend
 app.use(express.json()); // parse requests of content-type - application/json
 
-if(process.env.NODE_ENV === 'production') {
-  // Pointing to the Express server where the React build is.
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-  });
-}
+// if(process.env.NODE_ENV === 'production') {
+//   // Pointing to the Express server where the React build is.
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+//   });
+// }
 
-// app.get("/", (req, res) => {
-//   res.send("From BACKEND (Port 5000): Priority Based Family Physician Access");
-// });
+app.get("/", (req, res) => {
+  res.send("From BACKEND (Port 5000): Priority Based Family Physician Access");
+});
 
 // Handle POST requests to /api/patient
 app.post("/api/patient", async (req, res) => {
