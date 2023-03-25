6edfb9c6aad243efff092e1e778713a0a5d98873
Mustafa Wasif <mustafawasif@uvic.ca>
2023-03-25 00:54:16 -0700

    dynamically change prod and test env

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://priority-health.herokuapp.com/",
+  baseURL: process.env.REACT_APP_URL  || "http://localhost:5000/",
   // baseURL: "http://localhost:5000/",
   headers: {
     "Content-type": "application/json",
