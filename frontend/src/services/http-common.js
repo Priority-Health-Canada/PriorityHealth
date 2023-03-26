6c541fd5194de3cb457273cce74a1ee9257452a3
Aqeel Mozumder <aqeelmozumder@hotmail.com>
2023-03-26 01:20:08 -0700

    Production Fixes

--- frontend/src/services/http-common.js
+++ frontend/src/services/http-common.js
@@ -1,8 +1,22 @@
 import axios from "axios";
+import React from "react";
+
+function ReactIsInDevelomentMode() {
+  var result = "_self" in React.createElement("div") ? true : false;
+  return result;
+}
+
+var url = "";
+
+if (ReactIsInDevelomentMode) {
+  url = "http://localhost:5000/";
+} else {
+  url = process.env.REACT_APP_URL;
+}
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_URL  || "http://localhost:5000/",
-  // baseURL: "http://localhost:5000/",
+  baseURL: url,
+  //baseURL: "http://localhost:5000/",
   headers: {
     "Content-type": "application/json",
   },
--- server.ts
+++ server.ts
@@ -9,45 +9,48 @@ const app = express(); //Create an instance of express app
 app.use(cors()); //Allow different domains to access endpoints in backend
 app.use(express.json()); // parse requests of content-type - application/json
 
-// if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
+if (app.get("env") === "production") {
   // Pointing to the Express server where the React build is.
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("/*", (req, res) => {
+  app.get("/*", (req: express.Request, res: express.Response) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
-// }else{
-//   app.get("/", (req, res) => {
-//     res.send("From BACKEND (Port 5000): Priority Based Family Physician Access");
-//   });
-// }
-
-
+} else {
+  app.get("/", (req: express.Request, res: express.Response) => {
+    res.send(
+      "From BACKEND (Port 5000): Priority Based Family Physician Access"
+    );
+  });
+}
 
 // Handle POST requests to /api/patient
-app.post("/api/patient", async (req, res) => {
-  try {
-    console.log("FE data in BE: ", req.body);
-    // Parse the data from the request body
-    const patientData: PatientInfo = {
-      name: req.body.name,
-      dob: req.body.dob,
-      gender: req.body.gender,
-      email: req.body.email,
-    };
+app.post(
+  "/api/patient",
+  async (req: express.Request, res: express.Response) => {
+    try {
+      console.log("FE data in BE: ", req.body);
+      // Parse the data from the request body
+      const patientData: PatientInfo = {
+        name: req.body.name,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        email: req.body.email,
+      };
 
-    // Calculate Patient Metric Score based off their data input in the form
-    const pmScore = calcultePMS(patientData);
+      // Calculate Patient Metric Score based off their data input in the form
+      const pmScore = calcultePMS(patientData);
 
-    // Save the data to your database or file
-    await saveData({ ...patientData, pmScore: pmScore });
-    // Send a success response back to the client
-    res.send("Data saved to file");
-  } catch (error) {
-    // If there's an error, log it and send an error response to the client
-    console.log(error);
-    res.status(500).send("Error saving data to file");
+      // Save the data to your database or file
+      await saveData({ ...patientData, pmScore: pmScore });
+      // Send a success response back to the client
+      res.send("Data saved to file");
+    } catch (error) {
+      // If there's an error, log it and send an error response to the client
+      console.log(error);
+      res.status(500).send("Error saving data to file");
+    }
   }
-});
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
