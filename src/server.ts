460c635c818f89580066e8cb5d44e5a42e56a7e0
Aqeel Mozumder <aqeelmozumder@hotmail.com>
2023-03-27 13:04:18 -0700

    Fixes

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,10 @@ connectDB();
 
 app.use(cors()); //Allow different domains to access endpoints in backend
 app.use(express.json()); // parse requests of content-type - application/json
-app.use(express.static(path.join(__dirname, "../frontend/build"))); // Pointing to the Express server where the React build is.
 
 if (process.env.NODE_ENV?.trim() === "production") {
-  app.get("/index.html", (req: express.Request, res: express.Response) => {
+  app.use(express.static(path.join(__dirname, "../../frontend/build"))); // Pointing to the Express server where the React build is.
+  app.get("/*", (req: express.Request, res: express.Response) => {
     res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
   });
 } else if (process.env.NODE_ENV?.trim() === "development") {
