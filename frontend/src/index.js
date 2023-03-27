b2b289fd7f9e061d4b9fd94d6f7bcb18c851e168
Aqeel Mozumder <aqeelmozumder@hotmail.com>
2023-03-27 06:40:00 -0700

    Added Bootstrap in Frontend package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Redirect from "./Components/Redirect";
 import reportWebVitals from "./reportWebVitals";
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.render(
   <BrowserRouter>
